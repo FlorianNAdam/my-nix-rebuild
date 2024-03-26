3319264a5c165db6a4ce14cccc88bcc739d64ec4
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2024-03-26 13:13:09 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixosPath.default = "~/my-nixos"
   };
 
-  outputs = { self, nixpkgs, nixosPath }: let
+  outputs = { self, nixpkgs }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
