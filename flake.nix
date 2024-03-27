bc9263dee915e82d29e7559dcb519a2e4ec6ef1f
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2024-03-27 11:34:32 +0100

    add label sanitization

--- flake.nix
+++ flake.nix
@@ -1,48 +1,49 @@
 {
   description = "Sync with git and rebuild";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    packages = {
-      x86_64-linux = {
-        my-nix-rebuild = pkgs.writeShellScriptBin "my-nix-rebuild" ''
-          #!/usr/bin/env bash
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages = {
+        x86_64-linux = {
+          my-nix-rebuild = pkgs.writeShellScriptBin "my-nix-rebuild" ''
+            #!/usr/bin/env bash
 
-          # Usage: ./update-and-commit.sh <path> <label>
+            # Usage: ./update-and-commit.sh <path> <label>
 
-          NIXOS_PATH=$1          
+            NIXOS_PATH=$1          
 
-          if [ -z "$NIXOS_PATH" ]; then
-            echo "You must specify a path!"
-            exit 1
-          fi
+            if [ -z "$NIXOS_PATH" ]; then
+              echo "You must specify a path!"
+              exit 1
+            fi
 
-          LABEL=$2
+            LABEL=$2
 
-          if [ -z "$LABEL" ]; then
-            echo "You must specify a label!"
-            exit 1
-          fi
-          
-          set -e
+            if [ -z "$LABEL" ]; then
+              echo "You must specify a label!"
+              exit 1
+            fi
 
-          echo ">>>" $LABEL "<<<"
+            set -e
 
-          cd $NIXOS_PATH
-          git commit -a -m "$LABEL"
-          git push
-          sudo NIXOS_LABEL="$LABEL" nixos-rebuild switch --impure --flake $NIXOS_PATH
-        '';
+            # sync git repo
+            cd $NIXOS_PATH
+            git commit -a -m "$LABEL"
+            git push
+
+            # rebuild system
+            SANITIZED_LABEL=$(echo "$LABEL" | sed 's/ /_/g' | sed 's/[^a-zA-Z0-9:_\.-]//g')
+            sudo NIXOS_LABEL="$SANITIZED_LABEL" nixos-rebuild switch --impure --flake $NIXOS_PATH
+          '';
+        };
       };
-    };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-command;
-  };
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-nix-rebuild;
+    };
 }
 
